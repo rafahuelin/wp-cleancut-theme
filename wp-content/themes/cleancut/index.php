06d3cf4f9e6a3c33c995f032b15c6e5fa40e37d4
Rafa Huelin <it@engisa.com>
2017-07-30 11:32:17 +0100

    Blog posts & custom query

--- wp-content/themes/cleancut/index.php
+++ wp-content/themes/cleancut/index.php
@@ -1,71 +1,66 @@
 <?php get_header(); ?>
-
-<section class="row showcase animated fadeIn">
-    <div class="container">
-        <div class="col-md-12">
-            <div class="showcase-content">
-                <h1><?php echo get_theme_mod('showcase_heading', 'CleanCut Theme') ?></h1>
-                <p class="lead"><?php echo get_theme_mod('showcase_text', 'A Wordpress Theme By Rafa Huelin') ?></p>
-                <?php if(get_theme_mod('facebook_url', 'http://facebook.com') != '') : ?>
-                    <a class="btn btn-default btn-lg" href="<?php echo get_theme_mod('facebook_url', 'http://facebook.com'); ?>" target="_blank"><i class="fa fa-facebook fa-fw"></i> Facebook</a>
-                <?php endif; ?>
-                <?php if(get_theme_mod('twitter_url', 'http://twitter.com') != '') : ?>
-                    <a class="btn btn-default btn-lg" href="<?php echo get_theme_mod('twitter_url', 'http://twitter.com'); ?>" target="_blank"><i class="fa fa-twitter fa-fw"></i> Twitter</a>
-                <?php endif; ?>
-                <?php if(get_theme_mod('linkedin_url', 'http://linkedin.com') != '') : ?>
-                    <a class="btn btn-default btn-lg" href="<?php echo get_theme_mod('linkedin_url', 'http://linkedin.com'); ?>" target="_blank"><i class="fa fa-linkedin fa-fw"></i> Linkedin</a>
-                <?php endif; ?>
+    <section class="row title-bar">
+        <div class="container">
+            <div class="col-md-12">
+                <h1>Blog</h1>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+    <?php
+        $i = 0;
+    ?>
 
-<div class="section-a">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5 col-sm-6 animated fadeInLeft">
-                <hr class="section-heading-spacer">
-                <div class="clearfix"></div>
-                <h2 class="section-heading">Lorem ipsum dolor sit:</h2>
-                <p class="lead">Proin interdum metus in justo malesuada, non dignissim nunc porta. Pellentesque facilisis lorem cursus condimentum facilisis. Aenean viverra, ante quis tristique molestie</p>
-            </div>
-            <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                <img src="./img/pic1.jpg" class="img-responsive img-circle animated fadeInRight">
+    <?php while(have_posts()) : the_post(); ?>
+        <?php
+        $i++;
+        if($i % 2 !=0) {
+            // Odd Post
+            $section_class = 'section-a';
+            $left_class = 'col-lg-5 col-sm-6 animated fadeInLeft';
+            $right_class = 'col-lg-5 col-lg-offset-2 col-sm-6';
+            $img_class = 'img-circle img-responsive animated fadeInRight';
+        } else {
+            // Even Post
+            $section_class = 'section-b';
+            $left_class = 'col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6 animated fadeInRight';
+            $right_class = 'col-lg-5 col-sm-pull-6 col-sm-6';
+            $img_class = 'img-circle img-responsive animated fadeInLeft';
+        }
+        ?>
+        <div class="<?php echo $section_class; ?>">
+            <div class="container">
+                <div class="row">
+                    <div class="<?php echo $left_class; ?>">
+                        <hr class="section-heading-spacer">
+                        <div class="clearfix"></div>
+                        <h2 class="section-heading"><?php the_title(); ?>:</h2>
+                        <div class="meta">
+                            Posted on <?php the_time('F j, Y g:i a'); ?> By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a>
+                            in
+                            <?php
+                                $categories = get_the_category();
+                                $separator = ", ";
+                                $output = '';
+
+                                if($categories) {
+                                    foreach ($categories as $category) {
+                                        $output .= '<a href="'.get_category_link($category->term_id).'">' .$category->cat_name .'</a>'. $separator;
+                                    }
+                                }
+                                echo trim($output, $separator);
+                            ?>
+                        </div>
+                        <div class="excerpt"><?php the_excerpt(); ?></div>
+                        <a class="btn btn-primary" href="<?php the_permalink(); ?>">Read More</a>
+                    </div>
+                    <div class="<?php echo $right_class; ?>">
+                        <?php the_post_thumbnail('full', array(
+                            'class' => $img_class
+                        )); ?>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="section-b">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6 animated fadeInRight">
-                <hr class="section-heading-spacer">
-                <div class="clearfix"></div>
-                <h2 class="section-heading">Vivamus porta neque justo:</h2>
-                <p class="lead">Suspendisse at lacus condimentum, lobortis orci et, cursus sem. Donec id aliquet augue. Aliquam non aliquet urna, eu sagittis lacus</p>
-            </div>
-            <div class="col-lg-5 col-sm-pull-6 col-sm-6">
-                <img src="./img/pic2.jpg" class="img-responsive img-circle animated fadeInLeft">
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="section-a">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5 col-sm-6 animated fadeInLeft">
-                <hr class="section-heading-spacer">
-                <div class="clearfix"></div>
-                <h2 class="section-heading">Ut a dapibus felis:</h2>
-                <p class="lead">Ut auctor elementum lacus sit amet molestie. Ut a dapibus felis, non feugiat odio. Etiam sagittis aliquam felis, nec eleifend lectus bibendum et</p>
-            </div>
-            <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                <img src="./img/pic3.jpg" class="img-responsive img-circle animated fadeInRight">
-            </div>
-        </div>
-    </div>
-</div>
+    <?php endwhile; ?>
 
 <?php get_footer(); ?>
